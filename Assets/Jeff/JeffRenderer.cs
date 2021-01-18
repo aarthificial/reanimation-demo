9a752eff327acaaaef779a0b6298f1e44a4a90b2
aarthificial <aarthificial@gmail.com>
2021-01-18 02:27:41 +0100

    refactor: Clean up the renderer

--- Assets/Jeff/JeffRenderer.cs
+++ Assets/Jeff/JeffRenderer.cs
@@ -6,20 +6,24 @@ namespace Jeff
 {
     public class JeffRenderer : MonoBehaviour
     {
+        private static class Drivers
+        {
+            public const string Attack = "attack";
+            public const string FlipCompletion = "flipCompletion";
+            public const string HasHat = "hasHat";
+            public const string HitDirection = "hitDirection";
+            public const string IdleTransition = "idleTransition";
+            public const string IsGrounded = "isGrounded";
+            public const string JumpDirection = "jumpDirection";
+            public const string Movement = "movement";
+            public const string ShouldFlip = "shouldFlip";
+            public const string State = "state";
+            public const string StepEvent = "stepEvent";
+            public const string Temporary = "temporary";
+        }
+
         [SerializeField] private List<AudioClip> stepSounds = new List<AudioClip>();
         [SerializeField] private List<AudioClip> hitSounds = new List<AudioClip>();
-
-        private const string StateDriver = "state";
-        private const string MovementDriver = "movement";
-        private const string AttackDriver = "attack";
-        private const string IsGroundedDriver = "isGrounded";
-        private const string JumpDirectionDriver = "jumpDirection";
-        private const string HitDirectionDriver = "hitDirection";
-        private const string OneOffDriver = "_oneOff";
-        private const string JumpNumberDriver = "jumpNumber";
-        private const string FlipDriver = "flip";
-        private const string HatDriver = "hasHat";
-        private const string StepEventDriver = "stepEvent";
 
         private Reanimator _reanimator;
         private AudioSource _audioSource;
@@ -33,63 +37,56 @@ namespace Jeff
             _reanimator = GetComponent<Reanimator>();
             _controller = GetComponent<JeffController>();
             _audioSource = GetComponent<AudioSource>();
-
-            _reanimator.AddOneOffDriver(OneOffDriver);
         }
 
         private void OnEnable()
         {
-            _reanimator.AddListener(StepEventDriver, HandleStep);
+            _controller.HitStateEntered += HandleHitStateEntered;
+            _controller.HatTriggered += HandleHatTriggered;
             _reanimator.Ticked += UpdateColor;
-            _controller.Hat += HandleHat;
-            _controller.Hit += HandleHit;
+            _reanimator.AddListener(Drivers.StepEvent, HandleStep);
+            _reanimator.AddTemporaryDriver(Drivers.Temporary, Drivers.IdleTransition);
         }
 
         private void OnDisable()
         {
-            _reanimator.RemoveListener(StepEventDriver, HandleStep);
+            _controller.HitStateEntered -= HandleHitStateEntered;
+            _controller.HatTriggered -= HandleHatTriggered;
             _reanimator.Ticked -= UpdateColor;
-            _controller.Hat -= HandleHat;
-            _controller.Hit -= HandleHit;
+            _reanimator.RemoveListener(Drivers.StepEvent, HandleStep);
+            _reanimator.RemoveTemporaryDriver(Drivers.Temporary, Drivers.IdleTransition);
         }
 
         private void Update()
         {
             var velocity = _controller.Velocity;
 
-            _reanimator.Set(IsGroundedDriver, _controller.IsGrounded);
-            _reanimator.Set(HatDriver, _hasHat);
-
-            if (Mathf.Abs(_controller.DesiredMovementDirection.x) < 0.01f || Mathf.Abs(velocity.x) < 0.1f)
-                _reanimator.Set(MovementDriver, false);
-            else
-                _reanimator.Set(MovementDriver, true);
-
-            _reanimator.Set(JumpDirectionDriver, velocity.y > 0);
-
             _reanimator.Flip = _controller.FacingDirection < 0;
-            _reanimator.Set(StateDriver, (int) _controller.State);
-            _reanimator.Set(AttackDriver, _controller.AttackCompletion);
 
-            _reanimator.Set(JumpNumberDriver, _controller.IsJumping && !_controller.IsFirstJump);
-            _reanimator.Set(FlipDriver, _controller.JumpCompletion);
+            _reanimator.Set(Drivers.State, (int) _controller.State);
+            _reanimator.Set(Drivers.IsGrounded, _controller.IsGrounded);
+            _reanimator.Set(Drivers.Movement, _controller.IsMoving);
+            _reanimator.Set(Drivers.JumpDirection, velocity.y > 0);
+            _reanimator.Set(Drivers.ShouldFlip, _controller.IsJumping && !_controller.IsFirstJump);
+            _reanimator.Set(Drivers.FlipCompletion, _controller.JumpCompletion);
+            _reanimator.Set(Drivers.Attack, _controller.AttackCompletion);
 
             var velocityDirection = velocity.normalized;
             if (velocityDirection.y > 0.65f)
-                _reanimator.Set(HitDirectionDriver, 1);
+                _reanimator.Set(Drivers.HitDirection, 1);
             else if (velocityDirection.y < -0.65f)
-                _reanimator.Set(HitDirectionDriver, 2);
+                _reanimator.Set(Drivers.HitDirection, 2);
             else
-                _reanimator.Set(HitDirectionDriver, 0);
+                _reanimator.Set(Drivers.HitDirection, 0);
 
-            if (_reanimator.WillChange(IsGroundedDriver, 1))
+            bool didLandInThisFrame = _reanimator.WillChange(Drivers.IsGrounded, true);
+            bool didDashInThisFrame = _reanimator.WillChange(Drivers.State, (int) JeffState.Attack);
+
+            if (didLandInThisFrame)
                 HandleStep();
 
-            if (_reanimator.WillChange(IsGroundedDriver, 1)
-                || _reanimator.WillChange(StateDriver, (int) JeffState.Attack))
-            {
+            if (didLandInThisFrame || didDashInThisFrame)
                 _reanimator.ForceRerender();
-            }
         }
 
         private void UpdateColor()
@@ -106,12 +103,13 @@ namespace Jeff
             }
         }
 
-        private void HandleHat()
+        private void HandleHatTriggered()
         {
             _hasHat = !_hasHat;
+            _reanimator.Set(Drivers.HasHat, _hasHat);
         }
 
-        private void HandleHit()
+        private void HandleHitStateEntered()
         {
             if (hitSounds.Count > 0)
                 _audioSource.PlayOneShot(hitSounds[Random.Range(0, hitSounds.Count)]);
